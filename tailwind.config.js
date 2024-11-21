695ce1ea9d37816db8dd070aa0077dee1a9e7107
Samarth Nagar <samarthxma2009@gmail.com>
2024-11-21 12:53:03 +0530

    main new line

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-			fontfamily: {
-				geist: ["geist", "sans-serif"],
-			},
+			//fontfamily: {
+			//	geist: ["geist", "sans-serif"],
+			//},
 		},
 	},
 	plugins: [],
